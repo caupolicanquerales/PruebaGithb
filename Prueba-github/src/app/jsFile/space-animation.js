dd7a11ee48bbe765136fdd702a6bf4b6e3fcc59b
Caupolican <caupolicanquerales@gmail.com>
2021-06-28 17:41:18 -0300

    Cambios de forma de circulos

--- Prueba-github/src/app/jsFile/space-animation.js
+++ Prueba-github/src/app/jsFile/space-animation.js
@@ -4,8 +4,22 @@ export function setAnimation(){
     canvas= document.getElementById("space");
     let ctx= canvas.getContext("2d");
     ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(300,150);
+    ctx.moveTo(0,75);
+    ctx.lineTo(400,75);
     ctx.stroke();
-    console.log('hola canvas');
+    ctx.beginPath();
+    ctx.moveTo(150,0);
+    ctx.lineTo(150,200);
+    ctx.stroke();
+}
+
+export function setCircle(centerX,centerY,radius,color){
+    let ctx= canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(centerX,centerY,radius,0,2*Math.PI,false);
+    ctx.fillStyle=color;
+    ctx.fill();
+    ctx.lineWidth=1;
+    ctx.strokeStyle='#003300';
+    ctx.stroke();
 }
--- Prueba-github/src/app/spaceAnimation/space-animation/space-animation.component.ts
+++ Prueba-github/src/app/spaceAnimation/space-animation/space-animation.component.ts
@@ -12,7 +12,9 @@ export class SpaceAnimationComponent implements OnInit {
 
   ngOnInit(): void {
     animationSpace.setAnimation();
-    
+    animationSpace.setCircle(250,50,30,"green");
+    animationSpace.setCircle(50,100,40,"blue");
+    animationSpace.setCircle(120,130,30,"red");
   }
 
 }
